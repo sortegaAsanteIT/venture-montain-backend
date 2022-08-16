b1b307c7e89e97c03e473f1d0ee652beddd970e6
Sixto Ortega <ortegasixto7@gmail.com>
2022-08-16 15:03:35 -0400

    Adding project setup

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import compression from 'compression';
+import cors from 'cors';
+import express, { Response, Router } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { PORT } from './config/config';
+
+const app = express();
+app.use(morgan('dev'));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+app.use(cors());
+
+app.use(function (req, _, next) {
+  console.info(`REQUEST_BODY: ${JSON.stringify(req.body)}`);
+  next();
+});
+
+const router = Router();
+
+router.get('/', (_, res: Response) => {
+  res.status(200).json({ msg: 'API is working' });
+});
+
+app.use(router);
+app.listen(PORT, () => {
+  console.log(`-------\n App listening on port => ${PORT}\n-------`);
+});
